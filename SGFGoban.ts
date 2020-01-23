5b01f29d3488879bd98f415d2583ba19c8844963
ethan <ethanw123@gmail.com>
2020-01-23 16:16:20 -0500

    Need to work around webgl problem.

--- SGFGoban.ts
+++ SGFGoban.ts
@@ -1,6 +1,6 @@
 /*Create matrices and entities for sgf formating and stone placement*/
 
-function MakePointEntities(target: string, linenumber: number, width: number, depth: number){
+function MakePointEntities(target: string, linenumber: number, width: number, height: number, depth: number){
     let mainelement: any = document.getElementById(target);
     let letters: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's']; 
     
@@ -9,11 +9,11 @@ function MakePointEntities(target: string, linenumber: number, width: number, de
         for(let j =0; j < (linenumber-1); j++){
             let point = document.createElement('a-circle');
             point.setAttribute('id', letters[i]+letters[j]);
-            point.setAttribute('color', 'white');
-            point.setAttribute('radius', '.01');
+            point.setAttribute('color', 'blue');
+            point.setAttribute('radius', '.001');
             point.setAttribute('rotation', '270 0 0');
             //console.log( ((-width/2)+(width/linenumber)) + (i*(width/linenumber))+" "+0.01+" "+((-depth/2)+(depth/linenumber)) + (j*(depth/linenumber)));
-            point.setAttribute('position', ((-width/2)+(width/linenumber)) + (i*(width/linenumber))+" "+0.01+" "+((-depth/2)+(depth/linenumber)) + (j*(depth/linenumber)));
+            point.setAttribute('position', ((-width/2)+(width/(linenumber+1))) + (i*(width/(linenumber+1)))+" "+(height/2)+" "+((-depth/2)+(depth/(linenumber+1))) + (j*(depth/(linenumber+1))));
             mainelement.appendChild(point);
         }
 }
